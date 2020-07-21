c7c4762207c55954cfa9cbb7d7e6570e600ed58a
sarahriv <rivor003@umn.edu>
2020-07-21 13:27:41 -0700

    Updates

---
+++ precache-manifest.f15fff19a4383947fb5c4585f09a68d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce0a7c1cae893618e74545e3bc09ebc5",
+    "url": "/sarah-tries-react/index.html"
+  },
+  {
+    "revision": "e90582b69725068c00e0",
+    "url": "/sarah-tries-react/static/css/main.64cd0e1c.chunk.css"
+  },
+  {
+    "revision": "81dd70f028c06ec52425",
+    "url": "/sarah-tries-react/static/js/2.179d5027.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sarah-tries-react/static/js/2.179d5027.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e90582b69725068c00e0",
+    "url": "/sarah-tries-react/static/js/main.5ba684e5.chunk.js"
+  },
+  {
+    "revision": "1b5efd9e6a8992e77c5f",
+    "url": "/sarah-tries-react/static/js/runtime-main.b95a6301.js"
+  }
+]);
