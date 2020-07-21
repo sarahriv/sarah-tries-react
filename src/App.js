ba4b292bd5049a86028cbd3ab5094907fb004e5d
sarahriv <rivor003@umn.edu>
2020-07-21 13:20:55 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-const http = require('http');
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-const hostname = '127.0.0.1';
-const port = 3000;
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+export default App;
